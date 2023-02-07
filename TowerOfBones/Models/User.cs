6f58d21bd195b028e75a17256f43b014709f1616
EthanBarlevy <98473724+EthanBarlevy@users.noreply.github.com>
2023-02-06 22:23:20 -0700

    Added Darkly bootstrap theme. Started working on login.

---
+++ TowerOfBones/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TowerOfBones.Models
+{
+    public class User
+    {
+        [Key]
+        [Required]
+        private static int ID { get; set; }
+        [Required(ErrorMessage = "You must have a username.")]
+        [MaxLength(50)]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Your must have a password.")]
+        [MaxLength(255)]
+        [MinLength(8)]
+        public string Password { get; set; }
+        // there will probably need to be more things here like preferences and maybe stats or something
+
+        public User() { }
+
+    }
+}
