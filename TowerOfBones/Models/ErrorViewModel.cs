053c20d81212c9b90e147757aeedda3a76d8a1ca
yourespeakingtothemoon <manen@tutanota.com>
2023-02-06 19:34:50 -0700

    Add project files.

---
+++ TowerOfBones/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TowerOfBones.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
